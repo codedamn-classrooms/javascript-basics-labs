a7a801c98b87be706458a9aac61d5015c348b832
Mehul Mohan <mehulmpt@gmail.com>
2021-08-08 07:09:24 +0530

    bugfix

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// write your code below
+const two = 2
+// write the correct expression below
+const twoToThePower5 = 0
+
+const someNumber = 329826
+// write the correct expression here
+const someNumberToThePowerOf5 = 0
